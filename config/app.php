e84cc24b0c7f0a348463bce193f6227dd3504b34
mmauzerr <mmauzerr@gmail.com>
2017-09-13 13:22:38 +0200

    Setting Database and users Role and Permissions

--- config/app.php
+++ config/app.php
@@ -178,6 +178,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third Part Service Providers...
+         */
+        Laratrust\LaratrustServiceProvider::class,
     ],
 
     /*
@@ -210,6 +214,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
